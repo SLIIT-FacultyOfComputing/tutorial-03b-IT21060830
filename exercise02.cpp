48109a311578dba2486bd4591ea8537525af9839
IT21060830 <99633959+IT21060830@users.noreply.github.com>
2022-02-27 07:16:05 +0000

    Tutorial_3

--- exercise02.cpp
+++ exercise02.cpp
@@ -29,5 +29,9 @@ int main() {
     
     return 0;
 }
+int volume(int heigth, int width, int length)
+{
+  return heigth * length * width;
+}
 
 // Implement the Volume() function here
--- exercise03.cpp
+++ exercise03.cpp
@@ -5,6 +5,12 @@ using namespace std;
 int volume(int height, int width, int length);
 
 // 1. Define a structure called Box
+struct box
+{
+int heigth;
+int length;
+int width;
+}boxBox1, boxBox2;
 // have the integer data types Height, Width, Length
 
 // Do not change the main function
@@ -17,27 +23,31 @@ int main() {
     int totalVolume;
     
     // 4. Input the height, width, lenght of box1 and box2
-    cout << "Enter Box 1 Height : ";
-    cin >> Height of Box 1;
-    cout << "Enter Box 1 Width : ";
-    cin >> Width of Box 1;
-    cout << "Enter Box 1 Length : ";
-    cin >> Length of Box 1;
+    cout << "Enter Box 1 height : ";
+    cin >> boxBox1.height;
+    cout << "Enter Box 1 width : ";
+    cin >> boxBox1.width;
+    cout << "Enter Box 1 length : ";
+    cin >> boxBox1.length;
     
-    cout << "Enter Box 2 Height : ";
-    cin >> Height of Box 2;
-    cout << "Enter Box 2 Width : ";
-    cin >> Width of Box 2;
-    cout << "Enter Box 2 Length : ";
-    cin >> Length of Box 2;
+    cout << "Enter Box 2 height : ";
+    cin >> boxBox2.height;
+    cout << "Enter Box 2 width : ";
+    cin >> boxBox2.width;
+    cout << "Enter Box 2 length : ";
+    cin >> boxBox2.length;
     
     // 5. Replace the coding below to pass the Box type structure
-    totalVolume = volume( , , )
-             + volume( , , );
+    totalVolume = volume(boxBox1.height ,boxBox1.width ,boxBox1.length )
+             + volume(boxBox2.height ,boxBox2.width ,boxBox2.length );
     
     cout << "Volume of Box is " << totalVolume << endl;
     
     return 0;
+}
+int volume(int height, int width, int length);
+{
+  return heigth * width * length;
 }
 
 // Implement the functions here
